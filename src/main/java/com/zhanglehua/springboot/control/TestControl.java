cca69401529062d028f14466f9156c9b3ffb75af
lh <lh@windows10.microdone.cn>
2018-07-01 15:51:56 +0800

    测试代码提交

--- src/main/java/com/zhanglehua/springboot/control/TestControl.java
+++ src/main/java/com/zhanglehua/springboot/control/TestControl.java
@@ -1,19 +1,27 @@
 package com.zhanglehua.springboot.control;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zhanglehua.springboot.beans.Student;
 import com.zhanglehua.springboot.beans.Teacher;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-
 @RestController
-@Api(value = "UserInfo", description = "人员基本信息 ")
+@Configuration
 public class TestControl {
+	
+	 @Value("${name}")
+	 private  String name;
+	 @Value("${age}")
+	 private  String age;
 
-	@ApiOperation(value = "查询所有的人员信息", notes = "查询所有的人员信息")
+	@RequestMapping("/test")
+	public Object test() {
+		return "I am " + name + ", and i am " + age + " years old!";
+	}
+
 	@RequestMapping("/testjson")
 	public Object index() {
 		
